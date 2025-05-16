ce7ed2cf4c23d328c758714d972faa236692aa18
laura <s2210456016@fhooe.at>
2025-05-16 20:07:49 +0200

    initial-commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'testUser';
+        $user->email = 'test@gmail.com';
+        $user->password = bcrypt('secret');
+        $user->role = 'anbieter';
+        $user->ausbildung = 'KWM Bachelor';
+        $user->save();
+
+        $user1 = new User();
+        $user1->name = 'AngebotNehmer';
+        $user1->email = 'test2@gmail.com';
+        $user1->password = bcrypt('secret');
+        $user1->role = 'nehmer';
+        $user1->ausbildung = 'HTL Informatik';
+        $user1->save();
+    }
+}
