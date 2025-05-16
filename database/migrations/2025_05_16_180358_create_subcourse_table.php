cddde3744f060103d12e0bd59be345a372fd8771
scheuchenpflugc <clemens.scheuchenpflug@gmx.at>
2025-05-16 21:48:04 +0200

    all migrations

--- database/migrations/2025_05_16_180358_create_subcourse_table.php
+++ database/migrations/2025_05_16_180358_create_subcourse_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('subcourses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade'); // subkurs löschen wenn kurs gelöscht wird
+            $table->string('name');
         });
     }
 
--- database/seeders/CoursesTableSeeder.php
+++ database/seeders/CoursesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class CoursesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $course = new Course();
+        $course->name = 'Testkurs';
     }
 }
